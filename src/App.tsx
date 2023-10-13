f1ac69a4a18ce27ff72b351c1756363d541e29cb
IrinaEnotova <irinkayashina@gmail.com>
2023-10-13 13:19:54 +0400

    feat: add old routing method

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import Button from "./components/Button/Button";
 import { MouseEvent, useState } from "react";
 import Input from "./components/Input/Input";
+import { Routes, Route } from "react-router-dom";
+import { Menu } from "./pages/Menu/Menu";
+import { Cart } from "./pages/Cart/Cart";
+import { Error } from "./pages/Error/Error";
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -14,6 +18,15 @@ function App() {
       <Button onClick={addCounter}>{counter}</Button>
       <Button appearance="big">BIG</Button>
       <Input placeholder="" />
+      <div>
+        <a href="/">Menu</a>
+        <a href="/cart">Cart</a>
+      </div>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
