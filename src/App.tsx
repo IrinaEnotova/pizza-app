c9e0147dca4f4fcf136a02fbfce8d64992328c84
IrinaEnotova <irinkayashina@gmail.com>
2023-10-13 10:14:50 +0400

    feat: add components Button and Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import "./App.css";
 import Button from "./components/Button/Button";
+import { MouseEvent, useState } from "react";
+import Input from "./components/Input/Input";
 
 function App() {
+  const [counter, setCounter] = useState<number>(0);
+
+  const addCounter = (event: MouseEvent) => {
+    console.log(event);
+  };
+
   return (
     <>
-      <Button
-        onClick={() => {
-          console.log("click");
-        }}
-      >
-        BTN
-      </Button>
+      <Button onClick={addCounter}>{counter}</Button>
+      <Button appearance="big">BIG</Button>
+      <Input placeholder="" />
     </>
   );
 }
