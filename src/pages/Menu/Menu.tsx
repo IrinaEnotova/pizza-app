ea081a0f37e178186e90952dba262d35ae693a17
IrinaEnotova <irinkayashina@gmail.com>
2023-10-16 15:12:45 +0400

    feat: get product data from api

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,30 @@
 import { Heading } from "../../components/Heading/Heading";
 import { ProductCard } from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
+import { PREFIX } from "../../helpers/API";
+import { Product } from "../../interfaces/product.interface";
+import { useState, useEffect } from "react";
 import styles from "./Menu.module.css";
 
 export const Menu = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getMenu = async () => {
+    try {
+      const res = await fetch(`${PREFIX}/products`);
+      if (!res.ok) return;
+      const data: Product[] = await res.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <>
       <div className={styles["head"]}>
@@ -11,14 +32,17 @@ export const Menu = () => {
         <Search placeholder="Enter dish or ingridient" />
       </div>
       <div>
-        <ProductCard
-          id={1}
-          title="Delicious"
-          description="Salami, arugula, tomatoes, olives"
-          rating={4.5}
-          price={30}
-          image="./product-demo.png"
-        />
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.id}
+            title={product.name}
+            description={product.ingredients.join(", ")}
+            rating={product.rating}
+            price={product.price / 10}
+            image={product.image}
+          />
+        ))}
       </div>
     </>
   );
